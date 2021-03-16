7595b3ca5e4e1707e61a76f460bff9cba959d865
linqingfeng <lqfxz520@gmail.com>
2021-03-17 01:03:09 +0800

    .

--- lua/lqf/lsp.lua
+++ lua/lqf/lsp.lua
@@ -1,4 +1,7 @@
 local on_attach = require'completion'.on_attach
 require'lspconfig'.tsserver.setup{on_attach=on_attach}
 require'lspconfig'.vuels.setup{on_attach=on_attach}
+require'lspconfig'.cssls.setup{on_attach=on_attach}
+require'lspconfig'.html.setup{on_attach=on_attach}
+require'lspconfig'.jsonls.setup{on_attach=on_attach}
 
