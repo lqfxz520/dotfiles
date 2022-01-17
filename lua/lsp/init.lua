3ec76ee2c7746ee9c3a60603c30d978ce3a88848
linqingfeng <lqfxz520@gmail.com>
2022-01-17 21:21:49 +0800

    update

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -55,11 +55,6 @@ local servers = {
   jsonls = {},
   volar = {
     init_options = {},
-    settings = {
-      volar = {
-        lowPowerMode = true,
-      },
-    },
     on_new_config = function(new_config, new_root_dir)
       new_config.init_options.typescript.serverPath =
         get_typescript_server_path(
