aadaa495a0106298de9be10389bdf954a0030c0e
linqingfeng <lqfxz520@gmail.com>
2022-05-12 01:12:12 +0800

    update

--- lua/features/file_explorer.lua
+++ lua/features/file_explorer.lua
@@ -1,25 +1,26 @@
 local file_explorer = {}
 
 file_explorer.plugins = {
-	{
-		"kyazdani42/nvim-tree.lua",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("nvim-tree").setup({
-				disable_netrw = false,
-				update_cwd = true,
-				diagnostics = { enable = true },
-				view = { signcolumn = "auto" },
-				git = {
-					ignore = false,
-				},
-			})
-			local wk = require("which-key")
-			wk.register({
-				["<leader>pv"] = { "<cmd>NvimTreeToggle<cr>", "Toggle filetree" },
-			})
-		end,
-	},
+  {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('nvim-tree').setup({
+        disable_netrw = false,
+        update_cwd = true,
+        diagnostics = { enable = true },
+        view = { signcolumn = 'auto' },
+        git = {
+          ignore = false,
+        },
+      })
+      local wk = require('which-key')
+      wk.register({
+        ['<leader>pv'] = { '<cmd>NvimTreeFindFileToggle<cr>', 'Toggle filetree' },
+        ['<leader>ff'] = { '<cmd>NvimTreeFindFile<cr>', 'Find current page position' },
+      })
+    end,
+  },
 }
 
 return file_explorer
