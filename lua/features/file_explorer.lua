98c12d23f69ec3dba42258ef227dcc8812223010
linqingfeng <lqfxz520@gmail.com>
2022-07-28 00:57:20 +0800

    feat: update

--- lua/features/file_explorer.lua
+++ lua/features/file_explorer.lua
@@ -33,15 +33,51 @@ file_explorer.plugins[#file_explorer.plugins + 1] = {
   config = function()
     require('neo-tree').setup({
       close_if_last_window = true,
-        source_selector = {
-            winbar = true,
-            statusline = false
-        }
+      source_selector = {
+        winbar = true,
+        statusline = false,
+      },
+      window = {
+        mappings = {
+          ['<tab>'] = function(state)
+            local node = state.tree:get_node()
+            if require('neo-tree.utils').is_expandable(node) then
+              state.commands['toggle_node'](state)
+            else
+              state.commands['open'](state)
+              vim.cmd('Neotree reveal')
+            end
+          end,
+        },
+      },
+      filesystem = {
+        window = {
+          mappings = {
+            ['o'] = 'system_open',
+          },
+        },
+        commands = {
+          system_open = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            -- open file in default application in the background
+            local f = io.popen('wslpath -w ~')
+            print(f:read("*a"))
+            -- vim.api.nvim_command('!/mnt/c/WINDOWS/explorer.exe | wslpath -w' .. path)
+          end,
+        },
+      },
     })
     local wk = require('which-key')
     wk.register({
-      ['<leader>pv'] = { '<cmd>Neotree position=right action=focus reveal=true source=filesystem toggle=true<cr>', 'Toggle filetree' },
-      ['<leader>ff'] = { '<cmd>Neotree source=filesystem position=float reveal=true<cr>', 'Find current page position' },
+      ['<leader>pv'] = {
+        '<cmd>Neotree position=right action=focus reveal=true source=filesystem toggle=true<cr>',
+        'Toggle filetree',
+      },
+      ['<leader>ff'] = {
+        '<cmd>Neotree source=filesystem position=float reveal=true<cr>',
+        'Find current page position',
+      },
     })
   end,
 }
